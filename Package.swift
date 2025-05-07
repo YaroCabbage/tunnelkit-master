70517c96051ec6f83fb3858f3861f5f49d50ddb1
Flaviu Bura <flaviubuura@gmail.com>
2025-05-07 08:01:20 +0300

    feat: add proper wireguard

--- Package.swift
+++ Package.swift
@@ -42,9 +42,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "2.1.1"),
         .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.2.107"),
-//        .package(url: "https://git.zx2c4.com/wireguard-apple", .exact: Version("1.0.15-26")),
-//        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", exact: Version("1.0.17")),
-        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", revision: "b79f0f150356d8200a64922ecf041dd020140aa0")
+        .package(url: "https://github.com/AtomSDK/wireguard-apple.git", .branch("master")),
 //        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", branch: "develop")
 //        .package(name: "WireGuardKit", path: "../wireguard-apple")
     ],
